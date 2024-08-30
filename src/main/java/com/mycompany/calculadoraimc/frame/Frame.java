8a1834eccbb8a52182ea63c2805479dbba6f4516
rafaelnet29 <rafaelsouzanet29@outlook.com>
2024-08-30 19:45:00 -0300

    - Método Resultado criado para exibir os valores sobre o IMC - Botão calcular dispara evento chamando o método Resultado

--- src/main/java/com/mycompany/calculadoraimc/frame/Frame.java
+++ src/main/java/com/mycompany/calculadoraimc/frame/Frame.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.text.DecimalFormat;
+import java.text.MessageFormat;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -35,20 +36,21 @@ public final class Frame extends JFrame {
         btnSair = new JButton("Sair");
         txtResultado = new JTextArea();
         panel = new JPanel();
-        
+
         label.setBounds(40, 10, 250, 100);
-        label.setFont(new Font("ARIAl", Font.BOLD, 22));
+        label.setForeground(Color.red);
+        label.setFont(new Font("ARIAL", Font.BOLD, 22));
         labelAltura.setBounds(40, 50, 120, 100);
-        labelAltura.setFont(new Font("ARIAl", Font.BOLD, 18));
+        labelAltura.setFont(new Font("ARIAL", Font.BOLD, 18));
         txtAltura.setBounds(130, 90, 100, 20);
         labelPeso.setBounds(40, 90, 120, 100);
-        labelPeso.setFont(new Font("ARIAl", Font.BOLD, 18));
+        labelPeso.setFont(new Font("ARIAL", Font.BOLD, 18));
         txtPeso.setBounds(130, 130, 100, 20);
         btnCalcular.setBounds(40, 170, 120, 20);
-        btnSair.setBounds(170, 170, 90, 20);
-        txtResultado.setBounds(60, 220, 170, 40);
+        btnSair.setBounds(170, 170, 70, 20);
+        txtResultado.setBounds(40, 220, 200, 60);
         txtResultado.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.BLACK));
-        txtResultado.setFont(new Font("ARIAl", Font.BOLD, 22));
+        txtResultado.setFont(new Font("ARIAL", Font.BOLD, 14));
         txtResultado.setLineWrap(true);
         txtResultado.setEditable(false);
 
@@ -67,26 +69,56 @@ public final class Frame extends JFrame {
         this.pack();
         this.setVisible(true);
         this.setLocationRelativeTo(null);
-        
+
         CulcularIMC();
         Sair();
 
     }
-    
+
     public JButton CulcularIMC() {
         btnCalcular.addActionListener((e) -> {
-            double peso = Double.parseDouble(txtPeso.getText());
-            double altura = Double.parseDouble(txtAltura.getText());
-            
-            double imc = peso / (altura * altura);
-            txtResultado.setText("Seu IMC : " + new DecimalFormat("#.00").format(imc));
+            Resultado();
         });
         return btnCalcular;
     }
+
     public JButton Sair() {
         btnSair.addActionListener((e) -> {
             this.dispose();
         });
         return btnSair;
+    }
+
+    public Double Resultado() {
+        double peso = Double.parseDouble(txtPeso.getText());
+        double altura = Double.parseDouble(txtAltura.getText());
+
+        double imc = peso / (altura * altura);
+        if (imc < 18.5) {
+            txtResultado.setText(MessageFormat.format("Seu IMC : {0} {1}",
+                    new DecimalFormat("#.00").format(imc), " MAGREZA PREUCUPANTE!! "));
+            txtResultado.setForeground(Color.red);
+        } else if ((imc >= 18.5) && (imc < 24.9)) {
+            txtResultado.setText(MessageFormat.format("Seu IMC : {0} {1}",
+                    new DecimalFormat("#.00").format(imc), " NORMAL!! "));
+            txtResultado.setForeground(Color.blue);
+        } else if ((imc >= 25) && (imc < 29.9)) {
+            txtResultado.setText(MessageFormat.format("Seu IMC : {0} {1}",
+                    new DecimalFormat("#.00").format(imc), " SOBREPESO!! "));
+            txtResultado.setForeground(Color.green);
+        } else if ((imc >= 30) && (imc < 34.9)) {
+            txtResultado.setText(MessageFormat.format("Seu IMC : {0} {1}",
+                    new DecimalFormat("#.00").format(imc), " OBESIDADE GRAU #1!! "));
+            txtResultado.setForeground(Color.yellow);
+        } else if ((imc >= 35) && (imc < 39.9)) {
+            txtResultado.setText(MessageFormat.format("Seu IMC : {0} {1}",
+                    new DecimalFormat("#.00").format(imc), " OBESIDADE GRAU #2!! "));
+            txtResultado.setForeground(Color.DARK_GRAY);
+        } else {
+            txtResultado.setText(MessageFormat.format("Seu IMC : {0} {1}",
+                    new DecimalFormat("#.00").format(imc), " OBESIDADE GRAU #3!! "));
+            txtResultado.setForeground(Color.black);
+        }
+        return imc;
     }
 }
